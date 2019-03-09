52486f7dd70677deb4fe316cfe95df9afa45db70
ipsita95 <munnimallick14@gmail.com>
2019-03-09 11:10:54 +0530

    contact form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,29 @@ import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatGridListModule } from "@angular/material/grid-list";
 import { MatTableModule } from "@angular/material/table";
 import { MatListModule } from "@angular/material/list";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { FormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
 
 import { AppComponent } from "./app.component";
 import { from } from "rxjs";
-import { AboutComponent } from './about/about.component';
-import { AchivementsComponent } from './achivements/achivements.component';
-import { ContactComponent } from './contact/contact.component';
-import { EducationComponent } from './education/education.component';
-import { SkillComponent } from './skill/skill.component';
+import { AboutComponent } from "./about/about.component";
+import { AchivementsComponent } from "./achivements/achivements.component";
+import { ContactComponent } from "./contact/contact.component";
+import { EducationComponent } from "./education/education.component";
+import { SkillComponent } from "./skill/skill.component";
 
 @NgModule({
-  declarations: [AppComponent, AboutComponent, AchivementsComponent, ContactComponent, EducationComponent, SkillComponent],
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    AchivementsComponent,
+    ContactComponent,
+    EducationComponent,
+    SkillComponent
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -27,7 +39,12 @@ import { SkillComponent } from './skill/skill.component';
     MatSidenavModule,
     MatGridListModule,
     MatTableModule,
-    MatListModule
+    MatListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormControl, Validators, ReactiveFormsModule } from "@angular/forms";
 
 @Component({
-  selector: 'app-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  selector: "app-contact",
+  templateUrl: "./contact.component.html",
+  styleUrls: ["./contact.component.css"]
 })
-export class ContactComponent implements OnInit {
+export class ContactComponent {
+  email = new FormControl("", [Validators.required, Validators.email]);
 
-  constructor() { }
-
-  ngOnInit() {
+  getErrorMessage() {
+    return this.email.hasError("required")
+      ? "You must enter a value"
+      : this.email.hasError("email")
+      ? "Not a valid email"
+      : "";
   }
-
 }
